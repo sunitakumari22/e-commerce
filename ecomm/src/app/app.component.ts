3d3db5425b19c267c7d3436489f29da07d94f7bc
sunita kumari <139567148+SunitaKumari2004@users.noreply.github.com>
2025-02-13 21:17:25 +0530

    Added css files

--- ecomm/src/app/app.component.ts
+++ ecomm/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
